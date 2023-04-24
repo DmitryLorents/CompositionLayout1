89218637b1711073479db1bb69af2c96a076191c
DmitryLorents <zmey.task@gmail.com>
2023-04-24 14:24:18 +0600

    - create cell

--- CompositionLayout/ViewController.swift
+++ CompositionLayout/ViewController.swift
@@ -2,10 +2,51 @@
 //  ViewController.swift
 //  CompositionLayout
 //
-//  Created by Дмитрий Лоренц on 24.04.2023.
 //
 
 import UIKit
+
+class ListCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "listCellID"
+    
+    let label = UILabel()
+    let accessoryImageView = UIImageView()
+    let separatorView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+   func configure() {
+       label.translatesAutoresizingMaskIntoConstraints = false
+       label.adjustsFontForContentSizeCategory = true
+       label.font = .preferredFont(forTextStyle: .body)
+       contentView.addSubview(label)
+       
+       separatorView.translatesAutoresizingMaskIntoConstraints = false
+       separatorView.backgroundColor = .lightGray
+       contentView.addSubview(separatorView)
+       
+       selectedBackgroundView = UIView()
+       selectedBackgroundView?.backgroundColor = .lightGray.withAlphaComponent(0.3)
+       
+       accessoryImageView.translatesAutoresizingMaskIntoConstraints = false
+       contentView.addSubview(accessoryImageView)
+       
+       let rtl = effectiveUserInterfaceLayoutDirection == .rightToLeft
+       let chevronImageName = rtl ? "chevron.left" : "chevron.right"
+       let chevronImage = UIImage(systemName: chevronImageName)
+       accessoryImageView.image = chevronImage
+       accessoryImageView.tintColor = .lightGray.withAlphaComponent(0.7)
+   }
+    
+}
 
 class ViewController: UIViewController {
 
